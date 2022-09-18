81d787725c54a2c32ac273c7452da737f611ae43
18687448023 <113856685+18687448023@users.noreply.github.com>
2022-09-19 04:10:23 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx8771a120b2fd851d",
+  APP_ID: "wx59afd75d64c98216",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "0ef903d4e0112f3c998f78b4d09e1aba",
+  APP_SECRET: "a4eafeacb0272e03cbbeae723fd8808d",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -41,7 +41,7 @@ export const config = {
     poetry: false,
 
     /** 星座运势 */
-    horoscope: false,
+    horoscope: true,
 
     /** 生日消息和节日消息 */
     birthdayMessage: true,
@@ -62,9 +62,9 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oOQD56Spv4ExBMDi8QLLtf0qE_eY",
+      id: "oNtlj6CREnfS-bip0ZWGlEd_x6R4",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "QZq-CNVfXkRzO7zTbszzdG68daOByJ2_bOQzBYmxqqk",
+      useTemplateId: "FxO-q5KOg0lgvuldbiQMZNDKArpV1F7FdklPLiufGiw",
       // 所在省份
       province: "贵州",
       // 所在城市
